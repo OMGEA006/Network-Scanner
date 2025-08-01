1f5070a0da330efaf2da1729610f46fd864755ef
Jayse Monty <jmonty87019@uat.edu>
2025-08-01 20:21:50 +0000

    added first file

---
+++ NetScan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPORT_FILE=report.txt
+TARGET_IP=127.0.0.1
+
+echo "------------------------------------"
+echo "---- Generated Network Report ------"
+echo "---- For the Following IP address---"
+echo "----     $TARGET_IP        ----------"
+echo "------------------------------------"
+echo "||||||||||||||||||||||||||||||||||||"
+echo "-----------------------------------"
+echo "---- Ports and Services Found -----"
+echo "-----------------------------------"
+echo "----------- Port 20(FTP)-----------"
+echo "----------- Port 
+
+
